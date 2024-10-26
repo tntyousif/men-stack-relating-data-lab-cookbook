3e9c964878e2d154b56b313c72c90b8f35dff7e5
tntyousif <yousif02bh@gmail.com>
2024-10-26 21:24:16 +0300

    bugs fixing and the complet logic

--- controllers/ingredients.js
+++ controllers/ingredients.js
@@ -8,4 +8,31 @@ const Ingredient = require('../models/ingredient.js');
 
 // router logic will go here - will be built later on in the lab
 
+// GET /ingredients
+router.get('/', async (req, res) => {
+    try {
+      const ingredients = await Ingredient.find();
+      res.render('ingredients/index.ejs', { ingredients });
+    } catch (error) {
+      console.error(error);
+      res.redirect('/');
+    }
+});
+  
+// POST /ingredients
+router.post('/', async (req, res) => {
+    try {
+      const existingIngredient = await Ingredient.findOne({ name: req.body.name });
+      if (!existingIngredient) {
+        const newIngredient = new Ingredient(req.body);
+        await newIngredient.save();
+      }
+      res.redirect('/ingredients');
+    } catch (error) {
+      console.error(error);
+      res.redirect('/');
+    }
+});
+
+
 module.exports = router;
--- controllers/recipes.js
+++ controllers/recipes.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 const User = require('../models/user.js');
 const Recipe = require('../models/recipe.js');
+const Ingredient = require('../models/ingredient.js');
 
-// router logic will go here - will be built later on in the lab
 
 //landing page
 router.get("/", async (req, res) => {
@@ -15,8 +15,14 @@ router.get("/", async (req, res) => {
 })
 
 //new page render
-router.get("/new", async (req, res) => {
-   res.render('recipes/new.ejs');
+router.get('/new', async (req, res) => {
+    try {
+      const ingredients = await Ingredient.find();
+      res.render('recipes/new.ejs', { ingredients });
+    } catch (error) {
+      console.error(error);
+      res.redirect('/');
+    }
 });
 
 //add new recipes
@@ -34,10 +40,12 @@ router.post('/' , async ( req , res) =>{
 //show page route
 router.get('/:recipeId', async (req, res) => {
     try {
-        const recipe = await Recipe.findById(req.params.recipeId);
+        const recipe = await Recipe.findById(req.params.recipeId).populate('owner');
         res.locals.recipe = recipe;
-        res.render('recipes/show.ejs');
+        
+        const ingredients = await Ingredient.find();
 
+        res.render('recipes/show.ejs', { recipe, ingredients });
     } catch (error) {
         console.log(error);
         redirect('/')
@@ -87,13 +95,11 @@ router.delete('/:recipeId', async(req, res) => {
         }
         else {
           res.send("You are not allowed to delete this recipe")
-        }
-       
+        }  
     } catch(error) {
         console.log(error)
         res.redirect('/')
-    }
-      
+    }      
 });
 
 module.exports = router;
